dd1c94929375568f6d6689c7e7114b9e0583c094
coyotemanlol <codyleung@yahoo.com>
2016-11-06 16:16:55 -0800

    ArrayLab3s

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/29/50b696316da400161a8acf491e23350c
@@ -0,0 +1,10 @@
+
+public class ArrayLab3 {
+	
+		public static int[] sum(int [] arr1, int[] arr2) {
+			
+		}
+	
+
+
+}
--- ArraysLab3/ArrayLab3.java
+++ ArraysLab3/ArrayLab3.java
@@ -1,10 +1,34 @@
 
 public class ArrayLab3 {
-	
+		int[] arr1 = new int[3];
+		int[] arr1 = {1,2,3};
+		int[] arr1 = new int[]{1,2,3};
+		int[] arr2 = new int[3];
+		int[] arr2 = {3,4,5};
+		int[] arr2 = new int[]{1,2,3};
+		// sum
 		public static int[] sum(int [] arr1, int[] arr2) {
+			System.out.println(arr1.indexOf + arr2.indexOf);
+		}
+		
+		// append
+		public static int[] append(int[] arr, int num) {
 			
 		}
-	
-
+		
+		// remove
+		pulic static int[] remove(int[] arr, int idx) {
+			
+		}
+		
+		// sumEven
+		public static int sumEven(int[] arr) {
+			
+		}
+		
+		// rotateRight
+		public static void rotateRight(int[] arr) {
+			
+		}
 
 }
