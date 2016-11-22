0c102d30222cfd1afe21d91e8d49d438ab244089
coyotemanlol <codyleung@yahoo.com>
2016-11-21 22:27:56 -0800

    Frac Calc Part 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,28 +1,148 @@
+// Cody Leung - Frac Calc - Comp Sci 11/21/16
 package fracCalc;
+import java.util.Arrays; 
+import java.util.Scanner;
 
-public class FracCalc {
-
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
-    }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
-        
-        return "";
-    }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
-}
+	public class FracCalc {
+		
+		public static void main(String []args) {
+			Scanner fracReader = new Scanner(System.in);
+			
+			// Reading from System.in 
+			String input = fracReader.nextLine();
+			System.out.println(produceAnswer(input));
+			System.out.println(produceAnswer("5_3/4 + 6_5/8"));   //test
+		}
+		public static String produceAnswer(String input) {
+			if(input.indexOf("+")>=0) {
+				  String[] split1 = input.split(" ");
+				  String statement1 = split1[0];
+				   String statement2 = split1[1];
+				   String statement3 = split1[2];
+	               statement1 = statement1.trim();
+	               statement2 = statement2.trim();
+	               statement3 = statement3.trim();
+	               String whole = "0";
+	               String denominator = "0";
+	               String numerator = "0";
+	               if((statement3.indexOf("_")>=0)&&(statement3.indexOf("/")>=0)) {		 //when there is a whole number
+	            	    String[] split2 = statement3.split("_");
+	                    whole = split2[0];
+	                    String frac = split2[1];
+	                    String[] split3 = frac.split("/");
+	                    numerator = split3[0];
+	                    denominator = split3[1];
+	               }
+	               else if (statement3.indexOf("/")>=0) {
+	            	   String[] split2 = statement3.split("/");
+	                   whole = "0";
+	                   numerator = split2[0];
+	                   denominator = split2[1];
+	               }
+	               else {
+	                   whole = statement3;
+	                   denominator = "1";
+	               }
+	               //return statement3 + ("(Whole:" + whole +" Numerator:" + numerator +" Denominator:" + dem + ")");
+	               return "whole:" + whole +" numerator:" + numerator +" denominator:" + denominator;
+	               }
+		     else if (input.indexOf("-")>=0) { 			// equation = subtraction
+		            String[] split1 = input.split(" ");
+		            String statement1 = split1[0];
+		              String statement2 = split1[1];
+		              String statement3 = split1[2];
+		              statement1 = statement1.trim();
+		              statement2 = statement2.trim();
+		              statement3 = statement3.trim();
+		           String whole = "0";
+		           String denominator = "0";
+		           String numerator = "0";
+		              if((statement3.indexOf("_")>=0)&&(statement3.indexOf("/")>=0)) {			//When there is a whole number
+		               String[] split2 = statement3.split("_");
+		               whole = split2[0];
+		               String frac = split2[1];
+		               String[] split3 = frac.split("/");
+		               numerator = split3[0];
+		               denominator = split3[1];
+		              } 
+		              else if (statement3.indexOf("/")>=0) {
+		               String[] split2 = statement3.split("/");
+		               whole = "0";
+		               numerator = split2[0];
+		               denominator = split2[1];
+		              }
+		              else {
+		               whole = statement3;
+		               denominator = "1";
+		              }
+		              		//  returns statement3 + ("(Whole:" + whole +" Numerator:" + num +" Denominator:" + dem + ")");  
+		              return "whole:" + whole +" numerator:" + numerator +" denominator:" + denominator;
+		               
+		            } 
+		     else if(input.indexOf("/")>=0) { // equation = division
+	                String[] split1 = input.split(" ");
+	              String statement1 = split1[0];
+	              String statement2 = split1[1];
+	              String statement3 = split1[2];
+	              statement1 = statement1.trim();
+	              statement2 = statement2.trim();
+	              statement3 = statement3.trim();
+	           String whole = "0";
+	           String dem = "0";
+	           String numerator = "0";
+	              if((statement3.indexOf("_")>=0)&&(statement3.indexOf("/")>=0)){		// When there is a whole number present
+	               String[] split2 = statement3.split("_");
+	               whole = split2[0];
+	               String frac = split2[1];
+	               String[] split3 = frac.split("/");
+	               numerator = split3[0];
+	               dem = split3[1];
+	              } else if (statement3.indexOf("/")>=0){
+	                  String[] split2 = statement3.split("/");
+	                  whole = "0";
+	                  numerator = split2[0];
+	                  dem = split2[1];
+	                 }
+	                 else{
+	                  whole = statement3;
+	                  dem = "1";
+	                 }
+	            //Returns statement3 + ("(Whole:" + whole +" Numerator:" + num +" Denominator:" + dem + ")"); 
+	              return "whole:" + whole +" numerator:" + numerator +" denominator:" + dem;
+		     } 
+	     		else if(input.indexOf("*")>=0) {			 // equation = multiplication
+	                String[] split1 = input.split(" ");   
+	              String statement1 = split1[0];
+	              String statement2 = split1[1];
+	              String statement3 = split1[2];
+	              statement1 = statement1.trim();
+	              statement2 = statement2.trim();
+	              statement3 = statement3.trim();
+	           String whole = "0";
+	           String dem = "0";
+	           String numerator = "0";
+	              if((statement3.indexOf("_")>=0)&&(statement3.indexOf("/")>=0)){//when there is a whole number
+	               String[] split2 = statement3.split("_");
+	               whole = split2[0];
+	               String frac = split2[1];
+	               String[] split3 = frac.split("/");
+	               numerator = split3[0];
+	               dem = split3[1];
+	               
+	              } 
+	              else if (statement3.indexOf("/")>=0) {
+	               String[] split2 = statement3.split("/");
+	               whole = "0";
+	               numerator = split2[0];
+	               dem = split2[1];
+	              }
+	              else{
+	               whole = statement3;
+	               dem = "1";
+	              }
+	              //return statement3 + ("(Whole: " + whole +" Numerator:" + numerator +" Denominator:" + dem + ")");
+	              return "whole:" + whole +" numerator:" + numerator+" denominator:" + dem;
+	            }
+	        return "";
+	    }      
+	}
